d946870d63950ed439770db416c8ef76ba6dc930
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-01-28 16:47:59 +0100

    added basic structure and content to testovi.html

--- files/script.js
+++ files/script.js
@@ -1,3 +1,14 @@
 var prijava =()=>{
     window.location.href = 'pocetna.html';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('dodajTestDugme').addEventListener('click', function() {
+      // Redirect to a different HTML page
+      window.location.href = 'dodajTest.html';
+    });
+  });
+
+  function redirectToTest(testPage) {
+    window.location.href = testPage;
+}
