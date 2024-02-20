a91c380577097e1bc585898aec54da9af61f4aa1
sadyazz <97617659+sadyazz@users.noreply.github.com>
2024-02-20 20:52:01 +0100

    added startTest.html

--- student side/files/script.js
+++ student side/files/script.js
@@ -2,4 +2,14 @@ if(window.location.pathname.includes('testovi.html')){
     function  stariTestovi (){
         window.location.href = 'detaljiTesta.html';
       }
+      
+      function redirectToTest(){
+        window.location.href = 'startTest.html';
+      }
+
+}
+if(window.location.pathname.includes('startTest.html')){
+  function zapocniTest(){
+  window.location.href = 'test1.html';
+  }
 }
